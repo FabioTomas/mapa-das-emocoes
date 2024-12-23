1e0c10ced6bb082aa82dd070f1e7100e8f0c23f7
Fabio Tomas de Aquino <fabio.aquino@animaeducacao.com.br>
2024-12-23 10:58:01 -0300

    first commit

---
+++ src/emotions.js
@@ -0,0 +1,335 @@
+﻿var data = [
+    {
+        name: "Emoções", children: [
+            {
+                name: "Enjoado",
+                fill: "#8f8f8f",
+                children: [
+                    {
+                        name: "Desaprovação",
+                        children: [
+                            { name: "Julgamento" },
+                            { name: "Constrangido" },
+                        ]
+                    },
+                    {
+                        name: "Desapontado",
+                        children: [
+                            { name: "Chocado" },
+                            { name: "Revoltado" },
+                        ]
+                    },
+                    {
+                        name: "Terrível",
+                        children: [
+                            { name: "Nauseado" },
+                            { name: "Detestável" },
+                        ]
+                    },
+                    {
+                        name: "Rejeição",
+                        children: [
+                            { name: "Horrorizado" },
+                            { name: "Hesitação" },
+                        ]
+                    }
+                ]
+            },
+            {
+                name: "Triste",
+                fill: "#8fbde1",
+                children: [
+                    {
+                        name: "Magoado",
+                        children: [
+                            { name: "Constrangido" },
+                            { name: "Desapontado" },
+                        ]
+                    },
+                    {
+                        name: "Depressivo",
+                        children: [
+                            { name: "Inferior" },
+                            { name: "Vazio" },
+                        ]
+                    },
+                    {
+                        name: "Culpa", children: [
+                            { name: "Remorso" },
+                            { name: "Vergonha" },
+                        ]
+                    },
+                    {
+                        name: "Desespero",
+                        children: [
+                            { name: "Impotente" },
+                            { name: "Sofrimento" },
+                        ]
+                    },
+                    {
+                        name: "Vulnerávil",
+                        children: [
+                            { name: "Frágil" },
+                            { name: "Vitimado" },
+                        ]
+                    },
+                    {
+                        name: "Solidão",
+                        children: [
+                            { name: "Abandonado" },
+                            { name: "Isolado" },
+                        ]
+                    },
+                ]
+            },
+            {
+                name: "Feliz",
+                fill: "#ffff8f",
+                children: [
+                    {
+                        name: "Otimismo",
+                        children: [
+                            { name: "Inspiração" },
+                            { name: "Esperança" },
+                        ]
+                    },
+                    {
+                        name: "Confiança",
+                        children: [
+                            { name: "Intimidade" },
+                            { name: "Sensibiliade" },
+                        ]
+                    },
+                    {
+                        name: "Paz",
+                        children: [
+                            { name: "Agradecimento" },
+                            { name: "Amoroso" },
+                        ]
+                    },
+                    {
+                        name: "Poder",
+                        children: [
+                            { name: "Criatividade" },
+                            { name: "Coragem" },
+                        ]
+                    },
+                    {
+                        name: "Aceitação",
+                        children: [
+                            { name: "Reconhecimento" },
+                            { name: "Respeito" },
+                        ]
+                    },
+                    {
+                        name: "Orgulho",
+                        children: [
+                            { name: "Confiança" },
+                            { name: "Respeito" },
+                        ]
+                    },
+                    {
+                        name: "Interesse",
+                        children: [
+                            { name: "Indagação" },
+                            { name: "Curiosidade" },
+                        ]
+                    },
+                    {
+                        name: "Satisfação",
+                        children: [
+                            { name: "Felicidade" },
+                            { name: "liberdade" },
+                        ]
+                    },
+                    {
+                        name: "Diversão",
+                        children: [
+                            { name: "Atrevimento" },
+                            { name: "Excitação" },
+                        ]
+                    },
+
+                ]
+            },
+            {
+                name: "Surpresa",
+                fill: "#bca2d1",
+                children: [
+                    {
+                        name: "Excitação",
+                        children: [
+                            { name: "Energético" },
+                            { name: "Ansiedade" },
+                        ]
+                    },
+                    {
+                        name: "Espanto",
+                        children: [
+                            { name: "Temor" },
+                            { name: "Atônito" },
+                        ]
+                    },
+                    {
+                        name: "Confusão",
+                        children: [
+                            { name: "Perplexidade" },
+                            { name: "Desilusão" },
+                        ]
+                    },
+                    {
+                        name: "Assombro",
+                        children: [
+                            { name: "Desânimo" },
+                            { name: "Choque" },
+                        ]
+                    }
+                ]
+            },
+            {
+                name: "Mal",
+                fill: "#8ed9ae",
+                children: [
+                    {
+                        name: "Cansaço",
+                        children: [
+                            { name: "Disperso" },
+                            { name: "Sonoloento" },
+                        ]
+                    },
+                    {
+                        name: "Estresse",
+                        children: [
+                            { name: "Sem Controle" },
+                            { name: "Sobrecarregado" },
+                        ]
+                    },
+                    {
+                        name: "Ocupado",
+                        children: [
+                            { name: "Agradecimento" },
+                            { name: "Pressionado" },
+                        ]
+                    },
+                    {
+                        name: "Tédio",
+                        children: [
+                            { name: "Apátia" },
+                            { name: "Indiferença" },
+                        ]
+                    }
+                ]
+            },
+            {
+                name: "Medo",
+                fill: "#ffde8d",
+                children: [
+                    {
+                        name: "Assustado",
+                        children: [
+                            { name: "Indefeso" },
+                            { name: "Aterrorizado" },
+                        ]
+                    },
+                    {
+                        name: "Ansiedade",
+                        children: [
+                            { name: "Sobrecarregado" },
+                            { name: "Preocupação" },
+                        ]
+                    },
+                    {
+                        name: "Insegurança",
+                        children: [
+                            { name: "Inadequado" },
+                            { name: "Inferior" },
+                        ]
+                    },
+                    {
+                        name: "Fraco",
+                        children: [
+                            { name: "Incapaz" },
+                            { name: "Insignificante" },
+                        ]
+                    },
+                    {
+                        name: "Rejeição",
+                        children: [
+                            { name: "Excluido" },
+                            { name: "Reprimido" },
+                        ]
+                    },
+                    {
+                        name: "Ameaçado",
+                        children: [
+                            { name: "Nervoso" },
+                            { name: "Exposto" },
+                        ]
+                    }
+                ]
+            },
+            {
+                name: "Raiva",
+                fill: "#ff8f8e",
+                children: [
+                    {
+                        name: "Desapontado",
+                        children: [
+                            { name: "Traido" },
+                            { name: "Resentido" },
+                        ]
+                    },
+                    {
+                        name: "Humilhação",
+                        children: [
+                            { name: "Desrespeitado" },
+                            { name: "Ridicularizado" },
+                        ]
+                    },
+                    {
+                        name: "Amargura",
+                        children: [
+                            { name: "Indignado" },
+                            { name: "Violado" },
+                        ]
+                    },
+                    {
+                        name: "Ensandecido",
+                        children: [
+                            { name: "Fúria" },
+                            { name: "Inveja" },
+                        ]
+                    },
+                    {
+                        name: "Agressivo",
+                        children: [
+                            { name: "Provocador" },
+                            { name: "Hostil" },
+                        ]
+                    },
+                    {
+                        name: "Frustração",
+                        children: [
+                            { name: "Enfurecido" },
+                            { name: "Aborrecido" },
+                        ]
+                    },
+                    {
+                        name: "Reservado",
+                        children: [
+                            { name: "Retraido" },
+                            { name: "Entorpecido" },
+                        ]
+                    },
+                    {
+                        name: "Critico",
+                        children: [
+                            { name: "Cético" },
+                            { name: "Arrogante" },
+                        ]
+                    }
+                ]
+            },
+        ]
+    }
+];
