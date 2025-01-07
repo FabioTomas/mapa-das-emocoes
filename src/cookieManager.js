bdd0ca87978d2152d8d85d23022150393d569efd
Fabio Tomas de Aquino <fabio.aquino@animaeducacao.com.br>
2025-01-07 17:02:47 -0300

    Configuração do caminho dos itens da navbar e correção de acesso ao objeto chart

--- src/cookieManager.js
+++ src/cookieManager.js
@@ -1,22 +1,21 @@
-﻿
-function getCookie() {
-    const cookieName = 'RegisteredEmotions';
-    let name = cookieName + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            var cookie = c.substring(name.length, c.length);
-            if (cookie !== "") {
-                cookie = JSON.parse(cookie);
-            }
-            return cookie;
-        }
+﻿function getCookie() {
+  const cookieName = "RegisteredEmotions";
+  let name = cookieName + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
     }
-    document.cookie = cookieName + "=" 
-    return "";
+    if (c.indexOf(name) == 0) {
+      var cookie = c.substring(name.length, c.length);
+      if (cookie !== "") {
+        cookie = JSON.parse(cookie);
+      }
+      return cookie;
+    }
+  }
+  document.cookie = cookieName + "=";
+  return "";
 }
